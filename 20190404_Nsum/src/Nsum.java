e264c489a985a7310ee8d0b45f1a0b05327ad8a0
unknown <s2018s05@e-mirim.hs.kr>
2019-04-04 19:55:31 +0900

    Nsum

---
+++ 20190404_Nsum/src/Nsum.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Nsum {
+
+	public static void main(String[] args) {
+		//String으로 입력받을 것(입력한 것의 길이를 알아야함)
+		System.out.print("원하는 숫자를 입력하세요 : ");
+		Scanner scanner=new Scanner(System.in);
+		String number=scanner.nextLine();
+		int length=number.length();
+		int sum=0;
+		for(int i=0;i<length;i++) {
+			char n=number.charAt(i); //한글자씩 가져와서 숫자로 바꾸자, 0~자릿수-1까지 반복
+			sum+=n-'0'; //sum을 더하자
+		}
+		//더한값을 출력
+		System.out.println(sum);
+	}
+}
