bb1bf4ba198208cc340df7ace943fc80f9ec79fe
unknown <s2018s05@e-mirim.hs.kr>
2019-05-07 19:47:22 +0900

    JCombo

--- 20190430_GUI/src/GUITest2_JComboBox.java
+++ 20190430_GUI/src/GUITest2_JComboBox.java
@@ -1,27 +1,31 @@
-import java.awt.Color;
 import java.awt.Dimension;
 
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class JTextField_Yaho {
+public class GUITest2_JComboBox {
 
 	public static void main(String[] args) {
 		JFrame frame=new JFrame("창이름");
 		frame.setLocation(500,300);
 		frame.setPreferredSize(new Dimension(300,200));
 
+		JLabel label=new JLabel("태어난 달 : ");
 		JPanel panel=new JPanel();
-		JLabel label=new JLabel("야호");
-		JTextField tf=new JTextField("야호");
+		String[] picnics= {"1월","2월","3월","4월","5월","6월","7월","8월","9월","10월"};
+		JComboBox<String> cb=new JComboBox<>(picnics);
+		
+		cb.addItem("11월");
+		cb.addItem("12월");
 		
 		panel.add(label);
-		panel.add(tf);
+		panel.add(cb);
 		frame.add(panel);
 		
-		//frame.pack();
+	    frame.pack();
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
